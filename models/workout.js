2493a7c8324b45ae8f0e8e48c39a650f6169790b
Boaardz56 <jackie.lopez.56@gmail.com>
2020-07-31 10:08:31 -0400

    worked on models folders

--- models/workout.js
+++ models/workout.js
@@ -1,9 +1,52 @@
-//dat (type: Date)
-//exercise (type -> [])
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+  date: {
+   //date (type: Date)   
+    type: Date,
+   //get default but do NOT call the
+    default: Date.now
+  },
+   //exercises (type -> [])
+  exercises: [
     // type (type -> string)
+    {
+    type: {
+        type: String,
+        trim: true,
+        required: "Enter an exercise type"
+    },
     // name (type -> string)
+    name: {
+        type: String,
+        trim: true,
+        required: "Enter an exercise name"
+    },
     // duration (type -> number)
+    duration: {
+        type: Number
+    },
     // weight (type -> number)
+    weight: {
+        type: Number
+    },
     // reps (type -> number)
+    reps: {
+        type: Number
+    },
     // sets (type -> number)
+    sets: {
+        type: Number
+    },
     // distance (type -> number)
+    distance: {
+        type: Number
+    }
+   }
+  ] 
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
