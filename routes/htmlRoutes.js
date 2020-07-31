a6cfacbfe7abd172d9adbd22cecc976ed6d59519
Boaardz56 <jackie.lopez.56@gmail.com>
2020-07-31 10:43:03 -0400

    added in html routes and joined paths

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,8 +1,18 @@
-//index.html ("/")
+const router = require("express").Router();
+const path = require("path");
 
+
+//index.html ("/")
+router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./index.html"));
+  });
 
 //exercise.html ("/exercise")
-
-
+router.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "./exercise.html"));
+  });
 
 //stats.html ("/stats")
+outer.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "./stats.html"));
+  });
